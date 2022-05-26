34bd4f3f76bba102d21140c672ee3f0c84e92858
Josipa Bagarić <jbagaric20@foi.hr>
2022-05-26 22:17:50 +0200

    prvi commit Dodani gumbi btnAdd, btnChange, btnDelete i data grid view dgvDescriptiveData

---
+++ Software/Signal Localization v1.0/FrmDescriptiveData.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Signal_Localization_v1._0
+{
+    partial class FrmDescriptiveData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvDescriptiveData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDescriptiveData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(533, 333);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(84, 33);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(634, 333);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(84, 33);
+            this.btnChange.TabIndex = 1;
+            this.btnChange.Text = "Promijeni";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(634, 375);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(84, 33);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Obriši";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dgvDescriptiveData
+            // 
+            this.dgvDescriptiveData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDescriptiveData.Location = new System.Drawing.Point(53, 70);
+            this.dgvDescriptiveData.Name = "dgvDescriptiveData";
+            this.dgvDescriptiveData.RowHeadersWidth = 51;
+            this.dgvDescriptiveData.RowTemplate.Height = 24;
+            this.dgvDescriptiveData.Size = new System.Drawing.Size(665, 245);
+            this.dgvDescriptiveData.TabIndex = 3;
+            // 
+            // FrmDescriptiveData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDescriptiveData);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "FrmDescriptiveData";
+            this.Text = "FrmDescriptiveData";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDescriptiveData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvDescriptiveData;
+    }
+}
+
