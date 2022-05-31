addb89201e11d3602531c4abfe6657a8505896b7
Josipa Bagarić <jbagaric20@foi.hr>
2022-05-31 11:54:25 +0200

    Implementacija hijerarhije

---
+++ Software/Signal Localization/Models/Osoba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signal_Localization.Models
+{
+    public abstract class Osoba : Object
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public override string ToString()
+        {
+            return Ime + " " + Prezime;
+        }
+
+
+    }
+}
---
+++ Software/Signal Localization/Models/Zatvorenik.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signal_Localization.Models
+{
+    public class Zatvorenik : Osoba
+    {
+        public string DatumPritvora { get; set; }
+    }
+}
