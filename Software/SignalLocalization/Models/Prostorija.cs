2ab7adc651e9afdef9d718a3ff20cfec1c72b741
Josipa Bagarić <jbagaric20@foi.hr>
2022-06-16 12:18:00 +0200

    Napravljene klase Osoba.cs, Zatvorenik.cs, Prostorija.cs

---
+++ Software/SignalLocalization/Models/Prostorija.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalLocalization.Models
+{
+    public class Prostorija
+    {
+        public int Id { get; set; }
+        //public string Vrsta { get; set; }
+        public int Broj { get; set; }
+        public int Velicina { get; set; }
+        public int BrojKreveta { get; set; }
+        public int Kat { get; set; }
+        public int IdZatvorenika { get; set; }
+    }
+}
